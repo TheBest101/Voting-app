c7ffd84751f124f632bf60da86a228f2f9f2cb12
TheBest101 <lava.mob1001@gmail.com>
2017-07-13 10:39:33 -0400

    hello

--- app/config/auth.js
+++ app/config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
   'twitterAuth' : {
     'consumerKey'       : process.env.TWITTER_KEY,
     'consumerSecret'    : process.env.TWITTER_SECRET,
-    'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    'callbackURL'       : 'http://https://morning-lowlands-33051.herokuapp.com/auth/twitter/callback'
   },
 }
